7259e05903a860959686806fca8a1d43bdbdfdfc
SkySpaceF <1164741239@qq.com>
2021-09-19 16:10:15 +0800

    这是软件工程第一次个人编程作业

--- 软工第一次个人编程.py
+++ 软工第一次个人编程.py
@@ -1,3 +1,7 @@
+# -*- coding = utf-8 -*-
+# @Time：2021/9/19 14:02
+# @Author:FuTianyu
+# @Software:PyCharm
 import re
 
 
@@ -17,21 +21,22 @@ def ReadFile(path):
         print("文件打开错误")
     return file_content
 
+
 # 将ReadFile处理的文件内容中空格删除，并进一步细化关键词
 # 输入ReadFile()处理后返回的列表，将其
 def SplitTxtByBlank(file_content):
-    key_list=[]
+    key_list = []
     for single_line in file_content:
-        single_word=single_line.split(' ')
-        single_word=list(filter(None,single_word))
+        single_word = single_line.split(' ')
+        single_word = list(filter(None, single_word))
         for element in single_word:
-            if element!='':
+            if element != '':
                 key_list.append(element)
     return key_list
 
 
-#提取文件中重要关键词
-#传入ReadFile()处理后返回的列表，返回以重要关键词组成的列表
+# 提取文件中重要关键词
+# 传入ReadFile()处理后返回的列表，返回以重要关键词组成的列表
 def GetExtractKeyList(file_content):
     list_dest = []
     for i in file_content:
@@ -48,42 +53,42 @@ def GetExtractKeyList(file_content):
     return list_dest
 
 
-#返回文件中C、CPP关键词的总数
-#传入ReadFile()处理文件后返回的列表，返回关键词的个数
+# 返回文件中C、CPP关键词的总数
+# 传入ReadFile()处理文件后返回的列表，返回关键词的个数
 def CountStdCKey(key_list):
-    std_CKey=['else if','char','double','enum','float','int','long',
-              'short','signed','struct','union','unsigned',
-              'void','for','do','while','break','continue',
-              'if','else','goto','switch','case','default',
-              'return','auto','extern','register','static',
-              'const','sizeof','typedef','volatile'
-              ]
-    num_key=0
+    std_CKey = ['else if', 'char', 'double', 'enum', 'float', 'int', 'long',
+                'short', 'signed', 'struct', 'union', 'unsigned',
+                'void', 'for', 'do', 'while', 'break', 'continue',
+                'if', 'else', 'goto', 'switch', 'case', 'default',
+                'return', 'auto', 'extern', 'register', 'static',
+                'const', 'sizeof', 'typedef', 'volatile'
+                ]
+    num_key = 0
     for element in key_list:
         if element in std_CKey:
-            num_key+=1
+            num_key += 1
     return num_key
 
 
-#返回case数目为元素的列表
-#传入重要关键词组成的列表列表，输出每组switch内的case数目
+# 返回case数目为元素的列表
+# 传入重要关键词组成的列表列表，输出每组switch内的case数目
 def CountCase(Extracted_Key_list):
-    #文件中有switch才能统计case
-    lth=len(Extracted_Key_list)
-    if(Extracted_Key_list.count('switch')):
-        num_case=[]
+    # 文件中有switch才能统计case
+    lth = len(Extracted_Key_list)
+    if (Extracted_Key_list.count('switch')):
+        num_case = []
         for i in range(lth):
             if Extracted_Key_list[i] == 'switch':
-                if (i+1) == lth or Extracted_Key_list[i+1] != 'case' :
+                if (i + 1) == lth or Extracted_Key_list[i + 1] != 'case':
                     num_case.append(0)
-                elif i < lth-1:
-                    offset=1
-                    if Extracted_Key_list[i+offset] == 'case':
-                        cnt=0
-                        while(i+offset < lth):
-                            if Extracted_Key_list[offset+i] == 'case' :
-                                cnt+=1
-                                offset+=1
+                elif i < lth - 1:
+                    offset = 1
+                    if Extracted_Key_list[i + offset] == 'case':
+                        cnt = 0
+                        while i + offset < lth:
+                            if Extracted_Key_list[offset + i] == 'case':
+                                cnt += 1
+                                offset += 1
                             else:
                                 break
                         num_case.append(cnt)
@@ -94,35 +99,24 @@ def CountCase(Extracted_Key_list):
         return [0]
 
 
-
-
-
-
-
-
-
 if __name__ == '__main__':
     print("请将测试文件放在与本脚本文件同一目录下")
-    path_level=input("请输入测试文件名称和等级,目前仅支持1、2级")
-    path_level_list=path_level.split(' ')
-    path=str(path_level_list[0])
-    level=int(path_level_list[-1])
-    if level>2 or level<0 :
+    path_level = input("请输入测试文件名称和等级,目前仅支持1、2级")
+    path_level_list = path_level.split(' ')
+    path = str(path_level_list[0])
+    level = int(path_level_list[-1])
+    if level > 2 or level < 0:
         print('等级输入错误！')
 
     file_content = ReadFile(path)
     key_list = SplitTxtByBlank(file_content)
 
-
-    if level==1:
+    if level == 1:
         print('total num: ', CountStdCKey(key_list))
 
     else:
         print('total num: ', CountStdCKey(key_list))
-        print('switch num: ',key_list.count('switch'))
+        print('switch num: ', key_list.count('switch'))
         Extracted_Key_list = GetExtractKeyList(key_list)
-        print('case num: ',end='')
-        print(*CountCase(Extracted_Key_list),sep=' ')
-
-
-
+        print('case num: ', end='')
+        print(*CountCase(Extracted_Key_list), sep=' ')
